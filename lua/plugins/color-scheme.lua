15f90186e5a6ffe721199503a30c9613d054da84
Simen Saegrov <simen.saegrov@fremtind.no>
2026-04-07 21:59:20 +0200

    move all plugins to separate files

---
+++ lua/plugins/color-scheme.lua
@@ -0,0 +1,21 @@
+---@module 'lazy'
+---@type LazySpec
+return { -- You can easily change to a different colorscheme.
+  -- Change the name of the colorscheme plugin below, and then
+  -- change the command in the config to whatever the name of that colorscheme is.
+  --
+  -- if you want to see what colorschemes are already installed, you can use `:telescope colorscheme`.
+  'catppuccin/nvim',
+  priority = 1000, -- Make sure to load this before all the other start plugins.
+  config = function()
+    ---@diagnostic disable-next-line: missing-fields
+    require('catppuccin').setup {
+      -- styles = {
+      --   comments = { italic = false }, -- Disable italics in comments
+      -- },
+    }
+
+    -- Load the colorscheme here.
+    vim.cmd.colorscheme 'catppuccin-nvim'
+  end,
+}
