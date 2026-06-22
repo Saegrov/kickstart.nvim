27e846c91f45a08e8bc0195136e94c2d881926ed
Simen Saegrov <simen.saegrov@fremtind.no>
2026-06-22 10:08:17 +0200

    Add key-bindings for next/prev quickfix-list

--- init.lua
+++ init.lua
@@ -130,6 +130,12 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Next Quickfix item (Alt+j)
+vim.keymap.set('n', '<C-u>', '<cmd>cnext<CR>zz', { desc = 'Next quickfix item', noremap = true, silent = true })
+
+-- Previous Quickfix item (Alt+k)
+vim.keymap.set('n', '<C-i>', '<cmd>cprevious<CR>zz', { desc = 'Previous quickfix item', noremap = true, silent = true })
+
 -- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
 -- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
 -- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
